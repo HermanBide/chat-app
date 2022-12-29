32da32728c2d0683a4d1c6cd47bf588cc6278d12
hermanb <bideherman@yahoo.com>
2022-12-29 15:56:10 -0800

    updated connection to socket.io and fixed event handlers

---
+++ server/db.js
@@ -0,0 +1,17 @@
+// const mongoose = require("mongoose");
+
+// const MONGODB_URI =
+//   process.env.PROD_MONGODB ||
+//   process.env.MONGODB_URI ||
+//   "mongodb://127.0.0.1:27017/goalPlannerDatabase";
+
+// mongoose
+//   .connect(MONGODB_URI)
+//   .then(() => console.log("Successfully connected to mongoDB."))
+//   .catch((e) => console.error("MongoDB Connection Error: ", e.message));
+
+// const db = mongoose.connection;
+
+// db.on("error", console.error.bind(console, "MongoDB connection error:"));
+
+// module.exports = db;
--- server/index.js
+++ server/index.js
@@ -2,14 +2,12 @@ const express = require('express');
 const app = express();
 const http = require('http');
 const cors = require('cors');
+const mongoose = require('mongoose');
 const { Server } = require('socket.io'); 
 
 const PORT = process.env.PORT || 4000
-
-app.use(cors()); // Add cors middleware
-
+app.use(cors()); 
 const server = http.createServer(app);
-
 const io = new Server(server, {
     cors: {
       origin: 'http://localhost:3000',
@@ -17,27 +15,44 @@ const io = new Server(server, {
     },
   });
 
+const CHAT_BOTTOM = 'ChatBot'
+
+let chatRoom = ''
+let allUsers = []
+
   io.on('connection', (socket) => {
     console.log(`User connected ${socket.id}`);
 
     socket.on('join_room', (data) => {
         const { username, room } = data; // Data sent from client when join_room event emitted
+        socket.join(data)
         socket.join(room); // Join the user to a socket room
-      });
+        console.log(`User with ID: ${socket.id} joined room: ${room}`)
+        let __createdtime__ = Date.now(); // Current timestamp
+        // Send message to all users currently in the room, apart from the user that just joined
+        socket.to(room).emit('receive_message', {
+          message: `${username} has joined the chat room`,
+          username: CHAT_BOT,
+          __createdtime__,
+        });
 
+        socket.emit('receive_message', {
+          message: `Welcome ${username}`,
+          username: CHAT_BOT,
+          __createdtime__,
+        });
 
-      let __createdtime__ = Date.now(); // Current timestamp
-      // Send message to all users currently in the room, apart from the user that just joined
-      socket.to(room).emit('receive_message', {
-        message: `${username} has joined the chat room`,
-        username: CHAT_BOT,
-        __createdtime__,
+        chatRoom = room;
+        allUsers.push({ id: socket.id, username, room });
+        chatRoomUsers = allUsers.filter((user) => user.room === room);
+        socket.to(room).emit('chatroom_users', chatRoomUsers);
+        socket.emit('chatroom_users', chatRoomUsers);
+     
       });
 
     socket.on('disconnect', () => {
         console.log(`User disconnected ${socket.id}`);
     });
-
   });
 
 
